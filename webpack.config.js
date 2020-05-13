af28f057d2c6a6fa99f789e772024e11ed2c3284
Irina Sass <wassa@li.ru>
2020-05-13 13:13:35 +0700

    make layout

--- webpack.config.js
+++ webpack.config.js
@@ -8,84 +8,87 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  entry: { 
-    index: './src/index/index.js'
-    // about: './src/about/about.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
-  },
-    
-  module: {
-    rules: [
-      { 
-        test: /\.js$/, 
-        use: { loader: "babel-loader" }, 
-        exclude: /node_modules/ 
-      },
-      {
-        test: /\.css$/i,
-        use: [
-            (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+    entry: {
+        index: './src/index/index.js',
+        about: './src/about/about.js',
+        analytics: './src/analytics/analytics.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: './js/[name].[chunkhash].js'
+    },
+
+    module: {
+        rules: [
             {
-                loader:'css-loader',
-                options: {
-                    importLoaders: 2
-                }
-            },    
-            'postcss-loader'
-        ]
-       },
-           {
-            test: /\.(png|jpg|gif|ico|svg)$/,
-            use: [
-                'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
-                {
-                    loader: 'image-webpack-loader',
-                    options: {
-                        bypassOnDebug: true, 
-                        disable: true,  
+                test: /\.js$/,
+                use: { loader: "babel-loader" },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    (isDev ? 'style-loader' : { loader: MiniCssExtractPlugin.loader, options: { publicPath: '../' } }),
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2
+                        }
                     },
-                },
-            ]
-        },
-        {
-            test: /\.(eot|ttf|woff|woff2)$/,
-            loader: 'file-loader?name=./vendor/[name].[ext]'
-        }
-       
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|ico|svg)$/,
+                use: [
+                    'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=./vendor/[name].[ext]'
+            }
+
         ]
     },
-    
+
     plugins: [
         new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
+            filename: './css/[name].[contenthash].css'
         }),
         new OptimizeCssAssetsPlugin({
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano'),
             cssProcessorPluginOptions: {
-                    preset: ['default'],
+                preset: ['default'],
             },
             canPrint: true
         }),
-        new HtmlWebpackPlugin({            
-            inject: false,             
-            template: './src/index/index.html', 
-            filename: 'index.html' 
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/index/index.html',
+            filename: 'index.html'
         }),
-        // new HtmlWebpackPlugin({            
-        //     inject: false,             
-        //     template: './src/about/about.html', 
-        //     filename: 'about.html' 
-        // }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/about/about.html',
+            filename: 'about.html'
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/analytics/analytics.html',
+            filename: 'analytics.html'
+        }),
         new WebpackMd5Hash(),
         new webpack.DefinePlugin({
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         })
-          
     ]
-
-
 }
