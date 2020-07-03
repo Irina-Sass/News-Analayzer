8559b258c50d361b2f60271a29016ac7f41dcf0b
Irina Sass <wassa@li.ru>
2020-07-03 23:36:56 +0700

    remove console.log

--- src/js/modules/NewsApi.js
+++ src/js/modules/NewsApi.js
@@ -5,7 +5,6 @@ export class NewsApi {
   }
 
   getNews(keyword, from, to, pageSize, language, sortBy) {
-    console.log(this.baseUrl);
     const _url = `${this.baseUrl}/v2/everything?q=${keyword}&from=${from}&to=${to}&pageSize=${pageSize}&language=${language}&sortBy=${sortBy}&apiKey=${this.apiKey}`;
     return fetch(_url, { method: 'GET' }).then((res) => {
       if (res.ok) {
